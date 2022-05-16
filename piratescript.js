79299f5162ea00832140b43b6918541781917feb
Will <104028913+potato-potarto@users.noreply.github.com>
2022-05-16 23:00:50 +0100

    Update piratescript.js

--- piratescript.js
+++ piratescript.js
@@ -1,6 +1,23 @@
 
 function piratize(input){
-
-    // add you code in here
+    let final = ''
+    let translatedWords = []
+    let words = input.split(' ')
+    for (const word of words){
+        if (word === 'pounds'){
+                translatedWords.push('doubloons')
+        }
+        else if (isNaN(word) == false){
+                translatedWords.push(word)
+        }
+        else {
+            let letters = word.split('')
+            let finalWord = letters.slice(1).join('') + letters[0] + 'arr'
+            translatedWords.push(finalWord)
+        }
+    }
+    
+return translatedWords.join(' ')
     
 } 
+
